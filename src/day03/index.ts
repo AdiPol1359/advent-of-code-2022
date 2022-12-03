25d2628b19a935bb51c8dddc110f6415f2d377e9
adipol1359 <adrian.polak20@gmail.com>
2022-12-03 12:59:01 +0100

    refactor: remove index.ts file

--- src/day03/index.ts
+++
@@ -1,8 +0,0 @@
-const foo = <T>(array: Array<T>) => {
-  const items = array.filter((value, index) => index % 2 === 0);
-
-  return items;
-};
-
-const arr: number[] = [1, 2, 3, 4];
-const res = foo(arr);
